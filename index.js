621ffc44c31a6565ad31d2d6dbed3fb867a6114d
R3b1rthDrole <49059573+R3b1rthDrole@users.noreply.github.com>
2021-04-24 02:03:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 //load discord
 const Discord = require('discord.js')
